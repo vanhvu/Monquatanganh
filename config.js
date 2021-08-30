3a0499f757efe288445260ac5e2af461ba5dedb4
vanhvu <89770112+vanhvu@users.noreply.github.com>
2021-08-30 15:22:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Crush à!',
+    introTitle: 'Anh à!',
     introDesc: `Trái đất vốn lạ thường 
     Mà sao em cứ đi nhầm đường 
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: 'Nhấp vào tym em nài',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnYes: 'Vẫn cứ là thích em <33',
+    btnNo: 'Không, Em gái à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
+    btnReply: 'Gửi cho em <3',
+    reply: 'Không yêu em thì yêu ai ',
+    mess: 'Em biết mà 🥰. Yêu anh nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h anh qua đón công chúa nhé!!!',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://mageplaza.com/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
