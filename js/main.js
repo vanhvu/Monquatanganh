4f5463f059581a3526f200c41a8c588b7a7e640b
vanhvu <89770112+vanhvu@users.noreply.github.com>
2021-09-09 14:57:39 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -103,7 +103,7 @@ $('#yes').click(function() {
         html: true,
         width: 900,
         padding: '3em',
-        html: "<input type='text' class='form-control' id='txtReason' onmousemove=textGenerate()  placeholder='...'>",
+        html: "<input type='text' class='form-control' id='txtReason' onmousemove=textGenerate()  placeholder='Nhớ em, muốn gặp em!'>",
         background: '#fff url("img/iput-bg.jpg")',
         backdrop: `
               rgba(0,0,123,0.4)
